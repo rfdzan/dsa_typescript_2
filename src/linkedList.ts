622e4810fa30c1d24c10f82c30f582b17ff3c821
rfdzan <hekodev.py@gmail.com>
2024-06-07 10:21:41 +0700

    linked list insertions, fixed types

--- src/linkedList.ts
+++ src/linkedList.ts
@@ -1,10 +1,10 @@
 class LinkedList<T> {
-  firstNode: Node<T> | null 
+  firstNode: Node<T> 
   constructor(node: Node<T>) {
     this.firstNode = node
   }
   read(index: number): T | undefined {
-    let currentNode = this.firstNode;
+    let currentNode: Node<T> | null | undefined = this.firstNode;
     let currentIndex = 0;
     while (currentIndex < index) {
       if (currentNode == null) {
@@ -16,7 +16,7 @@ class LinkedList<T> {
     return currentNode?.content
   }
   indexOf(needle: T): number | null {
-    let currentNode = this.firstNode;
+    let currentNode: Node<T> | null | undefined = this.firstNode;
     let currentIndex = 0;
     while (true) {
       if (currentNode?.content === needle) {
@@ -29,10 +29,32 @@ class LinkedList<T> {
       currentIndex++;
     }
   }
+  insertNode(at: number, data: T) {
+    let newNode = new Node(data);
+    // constant time for insertions at the beginning
+    if (at === 0) {
+      newNode.addNode(this.firstNode)
+      this.firstNode = newNode;
+      return;
+    }
+    let currentNode: Node<T> | null | undefined = this.firstNode;
+    let currentIndex = 0;
+    // going through the linked list to reach position
+    // right before the requested index
+    while (currentIndex < (at - 1)) {
+      currentNode = currentNode?.nextNode;
+      currentIndex++;
+    }
+    newNode.nextNode = currentNode?.nextNode;
+    if (currentNode == null) {
+      return;
+    }
+    currentNode.nextNode = newNode;
+  }
 }
 class Node<T> {
   data: T
-  nextNode: Node<T> | null
+  nextNode: Node<T> | null | undefined
   constructor(data: T) {
     this.data = data;
     this.nextNode = null
@@ -59,6 +81,9 @@ function main() {
   let linkedList = new LinkedList(firstNode);
   console.log(linkedList.read(3));
   console.log(linkedList.indexOf("bob"));
-  // nodeArr.forEach((node) => console.log(node.nextNode));
+  linkedList.insertNode(3, "long");
+  for (let i = 0; i<5; i++) {
+    console.log(linkedList.read(i));
+  }
 }
 main();
